0a09711f93c69c623f84f5082d6e0cfda0e483a1
Edgar Mueller <edgar.m.mueller@gmail.com>
2014-08-04 22:05:56 +0200

    wip

--- app/controllers/QBViewModel.scala
+++ app/controllers/QBViewModel.scala
@@ -3,10 +3,30 @@ package controllers
 import org.qbproject.api.schema.{QBJson, QBType}
 import play.api.libs.json._
 
+import scala.reflect.ClassTag
+
 /**
  * Created by Edgar on 26.05.2014.
  */
 object QBView {
+
+  trait QBSerializable[A] {
+    def toJson(a: A): JsObject
+  }
+
+  trait SerializableBox {
+    type T
+    val t : T
+    val showInst : QBSerializable[T]
+  }
+
+  object SerializableBox {
+    def apply[T0 : QBSerializable](t0 : T0) = new SerializableBox {
+      type T = T0
+      val t = t0
+      val showInst = implicitly[QBSerializable[T]]
+    }
+  }
 
   // TODO: or reference via val?
   case class QBViewPath(domainPath: String)
@@ -31,51 +51,85 @@ object QBView {
 
   case class QBViewModel(domainType: QBType, elements: QBViewElement*)
 
-  implicit def qbViewModelWriter: Writes[QBViewModel] = OWrites[QBViewModel] { viewModel =>
-    Json.obj(
-      "domainType" -> "person", // TODO: how to reference
-      "elements" -> viewModel.elements.map(qbViewElementWriter.writes)
-    )
+  def useSerializableBox(sb : SerializableBox) = {
+    import sb._
+    showInst.toJson(t)
   }
 
-  implicit def qbViewControlWriter: Writes[QBViewControl] = OWrites[QBViewControl] { viewControl =>
-    Json.obj(
-      "type" -> "Control",
-      "path" -> viewControl.path.domainPath,
-      "name" -> viewControl.name
-    )
+
+  import scala.reflect.runtime.{ universe => ru }
+  import scala.reflect.runtime.universe._
+
+  implicit def qbElementWriter = new QBSerializable[QBViewElement] {
+    override def toJson(q: QBViewElement): JsObject = { case q =>
+      val clazz = implicitly[ClassTag[QBViewElement]].runtimeClass
+      val x = implicitly[QBSerializable[clazz.type]]
+      x.toJson(q)
+    }
   }
 
-  implicit def qbViewLabelWriter: Writes[QBLabel] = OWrites[QBLabel] { label =>
-    Json.obj(
-      "type" -> "Label",
-      "text" -> label.text
-    )
+  def paramInfo[T: TypeTag](x: T): Type = {
+    typeOf[T] match { case TypeRef(a, b, args) => a }
   }
 
-  implicit def qbViewGroupWriter: Writes[QBGroup] = OWrites[QBGroup] { group =>
-    Json.obj(
-      "type" -> "Group",
-      "name" -> group.name,
-      "elements" -> Json.arr(
-        group.elements.map(qbViewElementWriter.writes)
+  def getType[T](clazz: Class[T]): ru.Type = {
+    val runtimeMirror =  ru.runtimeMirror(clazz.getClassLoader)
+    runtimeMirror.classSymbol(clazz).toType
+  }
+
+  implicit def qbViewModelWriter = new QBSerializable[QBViewModel] {
+    def toJson(v: QBViewModel) = {
+      Json.obj(
+        "domainType" -> "person", // TODO: how to reference
+        "elements" -> v.elements.map(el => SerializableBox(el)).map(useSerializableBox)
       )
-    )
+    }
   }
 
-  implicit def qbViewLayoutWriter: Writes[QBViewLayout] = OWrites[QBViewLayout] { layout =>
-    Json.obj(
-      "type" -> layout.getClass.getSimpleName,
-      "elements" -> Json.arr(
-        layout.elements.map(qbViewElementWriter.writes)
+  implicit def qbViewControlWriter = new QBSerializable[QBViewControl] {
+    override def toJson(viewControl: QBViewControl): JsObject = {
+      Json.obj(
+        "type" -> "Control",
+        "path" -> viewControl.path.domainPath,
+        "name" -> viewControl.name
       )
-    )
+    }
   }
 
-  implicit def qbViewElementWriter: Writes[QBViewElement] = OWrites[QBViewElement] {
-    case ctrl: QBViewControl => qbViewControlWriter.writes(ctrl).as[JsObject]
-    case grp: QBGroup => qbViewGroupWriter.writes(grp).as[JsObject]
-    case lbl: QBLabel => qbViewLabelWriter.writes(lbl).as[JsObject]
-    case layout: QBViewLayout => qbViewLayoutWriter.writes(layout).as[JsObject]
+  implicit def qbViewLabelWriter = new QBSerializable[QBLabel] {
+    override def toJson(label: QBLabel): JsObject = {
+      Json.obj(
+        "type" -> "Label",
+        "text" -> label.text
+      )
+    }
+  }
+
+  implicit def qbViewGroupWriter = new QBSerializable[QBGroup] {
+    override def toJson(group: QBGroup): JsObject = {
+      Json.obj(
+        "type" -> "Group",
+        "name" -> group.name,
+        "elements" -> Json.arr(
+          group.elements.map(el => SerializableBox(el)).map(useSerializableBox)
+        )
+      )
+    }
+  }
+
+  implicit def qbViewLayoutWriter = new QBSerializable[QBViewLayout] {
+    override def toJson(layout: QBViewLayout): JsObject = {
+      Json.obj(
+        "type" -> layout.getClass.getSimpleName,
+        "elements" -> Json.arr(
+          layout.elements.map(el => SerializableBox(el)).map(useSerializableBox)
+        )
+      )
+    }
+  }
+
+  implicit def qbViewElementWriter[Q : QBSerializable]: Writes[Q] = OWrites[Q] { q: Q =>
+    val m = implicitly[QBSerializable[Q]]
+    m.toJson(q)
   }
 }
